bf7d9358ab7c8cdeeda4df7a56c025095d6c240c
Votre Nom <votre.email@example.com>
2025-03-28 09:51:51 +0100

    Ajout de messages tests.

--- tests/tests_file.c
+++ tests/tests_file.c
@@ -136,8 +136,12 @@ int main() {
         return CU_get_error();
     }
 
+    CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
+    CU_basic_show_failures(CU_get_failure_list());
+    int failed_tests = CU_get_number_of_failures();
     CU_cleanup_registry();
+    return (failed_tests > 0) ? 1 : 0;
 
     return 0;
 }
