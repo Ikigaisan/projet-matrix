bc67c08f8b46e076f711f684b158d36c5f9bf8de
Votre Nom <votre.email@example.com>
2025-04-14 18:42:19 +0200

    J'ai un peu changé les fichiers de matrices pour que ça marche et qu'on puisse voir si le main marche.

--- help/generator_matrix.c
+++ help/generator_matrix.c
@@ -6,19 +6,24 @@
 #include "../headers/file.h"
 #include "../headers/matrix.h"
 
-int main(int argc, char **argv) {
-    // Modify here
+void make_file_matrix(char* filename){
     uint64_t m = 3;
     uint64_t n = 1;
     matrix *A = init_matrix(m, n);
-    A->values[0][0] = -2.0;
-    A->values[1][0] = 3.4;
-    A->values[2][0] = 1.7;
-    char *file_name = "matrix_A.bin";
-    // Stop modify
-
+    A->values[0][0] = rand()/2;
+    A->values[1][0] = rand()/2;
+    A->values[2][0] = rand()/2;
+    char *file_name = filename;
     FILE *file = fopen(file_name, "w+");
     write_matrix(A, file);
+    print_matrix(A);
     free(A);
     fclose(file);
+}
+
+int main(int argc, char **argv) {
+    printf("Matrix A :\n");
+    make_file_matrix("matrix_A.bin");
+    printf("Matrix B :\n");
+    make_file_matrix("matrix_B.bin");
 }
